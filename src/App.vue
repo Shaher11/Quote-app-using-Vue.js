94510b1287bfeaa90a42813cef5ad55aec8ba3ce
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-03-16 21:52:34 +0200

    Finish Adding progress bar with validation on the number of quotes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     <div class="row">
@@ -13,6 +14,7 @@
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
 import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 export default {
 
   data: function(){
@@ -28,6 +30,9 @@ export default {
   },
   methods:{
     newQuote(quote){
+      if(this.quotes.length >= this.maxQuotes){
+        return alert('Please delete one Quote first');
+      }
       this.quotes.push(quote);
     },
 
@@ -39,7 +44,8 @@ export default {
 
   components: {
     appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
     
 
